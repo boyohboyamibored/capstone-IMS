3e8a3f4ae2f42e93c4f8fbb1a04a36b38ccb97db
boyohboyamibored <myemail@email.com>
2024-11-21 13:49:19 +0000

    Added server, base template and stylesheet
    - Added server.py so the file can be run
    - Added base template, home page and stylesheet

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    """The default port is 5000, I used a different one
+    when running another flask app"""
+    app.run(debug=True, port=3000)
