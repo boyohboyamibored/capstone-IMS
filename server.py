0490da1f3f3d912c7ba9a8ae2d8ea283eae2c10f
boyohboyamibored <myemail@email.com>
2024-11-22 15:39:42 +0000

    Added some styling, with login and signup pages
    Added some a navbar basic styling, also added empty login and signup
    pages

--- server.py
+++ server.py
@@ -8,7 +8,23 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        return "<h1>Feature not yet available</h1>"
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        return "<h1>Feature not yet available</h1>"
+
+
 if __name__ == "__main__":
-    """The default port is 5000, I used a different one
+    """The default port is 5000, I use a different one
     when running another flask app"""
     app.run(debug=True)
