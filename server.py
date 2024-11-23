6bc5c93d060bcd82eec8851e03826c59c151cb69
boyohboyamibored <myemail@email.com>
2024-11-23 12:28:01 +0000

    Added message flashing, logout route and favicon

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
-from flask import Flask, redirect, render_template, request, url_for
+from datetime import timedelta
+
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+
+"""Generate a secret key with
+$ python -c 'import secrets; print(secrets.token_hex()) """
+app.secret_key = "supersecretkey"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 
 @app.route("/", methods=["GET"])
@@ -14,8 +21,15 @@ def login():
         return render_template("login.html")
     else:
         email = request.form.get("email")
+        flash("Login successful", "info")
         print(email, "has logged in")
-        return render_template("index.html")
+        return redirect(url_for("home"))
+
+
+@app.route("/logout", methods=["GET"])
+def logout():
+    flash("Logout sucessful", "info")
+    return redirect(url_for("home"))
 
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -23,9 +37,10 @@ def sign_up():
     if request.method == "GET":
         return render_template("signup.html")
     else:
+        flash("Account created successful", "info")
         email = request.form.get("email")
         print(email, "has signed up")
-        return render_template("index.html")
+        return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
